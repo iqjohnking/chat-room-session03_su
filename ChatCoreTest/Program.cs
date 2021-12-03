5162d2b55417465cb7c9310a48f851d28d15f13d
Inko_Su <iqjohnking@gmail.com>
2021-12-03 20:35:22 +0800

    data_packing_pratice_ver.1.0
    use the same method as  20_曾鐁賢

--- ChatCoreTest/Program.cs
+++ ChatCoreTest/Program.cs
@@ -5,48 +5,73 @@ namespace ChatCoreTest
 {
     internal class Program
     {
-        private static byte[] m_PacketData;//unPacket this, Add Length to package
+        private static byte[] m_PacketData;//unPack this, Add Length to package
         private static uint m_Pos;//it maens Length
-
+        private static byte[] l_PacketData;//this package store data
+        //use the same method as  20_曾鐁賢
         public static void Main()
         {
             m_PacketData = new byte[1024];
+            l_PacketData = new byte[1024];
             m_Pos = 0;
 
-            Write(109);
-            Write(109.99f);
-            Write("Hello!");
+            Write(109); //write int as byte in l_PacketData
+            Write(109.99f); //write float as byte in l_PacketData
+            Write("哈囉你好嗎!"); //write string as unicode as byte in l_PacketData
+            byte[] length = Getlength(m_Pos); //this package store length
 
-            Console.Write($"Output Byte array(length:{m_Pos}): ");
-            for (var i = 0; i < m_Pos; i++)
+
+
+            int l = 0;
+            while (l < m_Pos + 4)
+            {
+                if (l < 4)
+                {
+                    m_PacketData[l] = length[l]; //put length into m_PacketData
+                }
+                else
+                {
+                    m_PacketData[l] = l_PacketData[l - 4];//put data into m_PacketData
+                }
+                l++;
+            }
+
+            Console.WriteLine($"Output Byte array(length:{m_Pos}): ");
+            for (var i = 0; i < m_Pos + 4; i++)
             {
                 Console.Write(m_PacketData[i] + ", ");
             }
 
+            byte[] lengthread = new byte[4];
             byte[] intread = new byte[4];
             byte[] floatread = new byte[4];
             byte[] stringread = new byte[12];
 
             int j = 0;
-            while (j < m_Pos)
+            while (j < m_Pos + 4)
             {
-                if (j < 4)//0123
+                if (j < 4)//0 1 2 3
                 {
-                    intread[j] = m_PacketData[j];
+                    lengthread[j] = m_PacketData[j];
                 }
-                else if (j >= 4 && j < 8)//4567//8 9 10 11
+                else if (j >= 4 && j < 8)//4 5 6 7
                 {
-                    floatread[j - 4] = m_PacketData[j];
+                    intread[j - 4] = m_PacketData[j];
                 }
-                else if (j >= 12)//1213141516........
+                else if (j >= 8 && j < 12)//8 9 10 11
                 {
-                    stringread[j - 12] = m_PacketData[j];
+                    floatread[j - 8] = m_PacketData[j];
+                }
+                else if (j >= 16)//12 13 14 15//16........
+                {
+                    stringread[j - 16] = m_PacketData[j];
                 }
                 j++;
             }
 
             Console.WriteLine();
             Console.WriteLine($"Output what we read:");
+            Console.WriteLine(ReadLenght(lengthread));
             Console.WriteLine(ReadInt(intread));
             Console.WriteLine(ReadFloat(floatread));
             Console.WriteLine(ReadString(stringread));
@@ -95,11 +120,33 @@ namespace ChatCoreTest
             {
                 Array.Reverse(byteData);
             }
-
-            byteData.CopyTo(m_PacketData, m_Pos);
+            // Write2:first write into l_P, and let m_P = l_P.
+            byteData.CopyTo(l_PacketData, m_Pos);
             m_Pos += (uint)byteData.Length;
         }
 
+        private static byte[] Getlength(uint length)
+        {
+            byte[] length2Bytes = BitConverter.GetBytes(length);
+
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(length2Bytes);
+            }
+
+            return length2Bytes;
+        }
+
+        private static uint ReadLenght(byte[] byteData)
+        {
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(byteData);
+            }
+
+            uint byte2length = BitConverter.ToUInt32(byteData, 0);
+            return byte2length;
+        }
 
         private static int ReadInt(byte[] byteData)
         {
@@ -136,5 +183,4 @@ namespace ChatCoreTest
 
     }
 
-    //just for test
 }
